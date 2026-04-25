f9b6e51aa5d69b314d3db6995d54d5de0493a9d5
lucasfariascz <lucasfariasdacruz@gmail.com>
2026-04-24 23:43:48 -0300

    feat(api): add Swagger documentation for API endpoints

--- src/GoodHamburger.Api/Program.cs
+++ src/GoodHamburger.Api/Program.cs
@@ -8,10 +8,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "GoodHamburger API", Version = "v1" });
+});
 
 var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection")
                        ?? "Data Source=goodhamburguer.db";
@@ -27,13 +28,8 @@ using (var scope = app.Services.CreateScope())
     Db.Database.Migrate();
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
